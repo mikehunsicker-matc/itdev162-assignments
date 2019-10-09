232b8db77c9dc2f25e59e993513ad4554de27937
Mike Hunsicker <hunsickm@matc.edu>
2019-10-09 11:07:50 -0500

    Added api controller for Post entities

--- mike-hunsicker/API/Controllers/PostsController.cs
+++ mike-hunsicker/API/Controllers/PostsController.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Application.Posts;
+using System.Linq;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -11,12 +10,18 @@ namespace API.Controllers
   [ApiController]
   public class PostsController : ControllerBase
   {
-    private readonly IMediator mediator;
-    public PostsController(IMediator mediator) => this.mediator = mediator;
+    private readonly DataContext context;
 
-    public async Task<ActionResult<List<Post>>> List()
+    public PostsController(DataContext context)
     {
-      return await this.mediator.Send(new List.Query());
+      this.context = context;
+    }
+
+    // GET api/posts
+    [HttpGet]
+    public ActionResult<List<Post>> Get()
+    {
+      return this.context.Posts.ToList();
     }
   }
 }
--- mike-hunsicker/API/Startup.cs
+++ mike-hunsicker/API/Startup.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Application.Posts;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,7 +29,6 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMediatR(typeof(List.Handler).Assembly);
       services.AddCors(opt =>
       {
         opt.AddPolicy(CorsPolicy, policyBuilder =>
