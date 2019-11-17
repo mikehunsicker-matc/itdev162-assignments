0a6213bf7b1b85f091ac8288191fb830874734d4
Mike Hunsicker <hunsickm@matc.edu>
2019-11-17 13:25:12 -0600

    Completed Activity 10

--- mike-hunsicker/client/src/components/PostList/PostList.js
+++ mike-hunsicker/client/src/components/PostList/PostList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PostListItem from './PostListItem';
 
 const PostList = props => {
-  const { posts, clickPost, deletePost } = props;
+  const { posts, clickPost, deletePost, editPost } = props;
   return posts.map(post => (
     <PostListItem
       key={post.id}
       post={post}
       clickPost={clickPost}
       deletePost={deletePost}
+      editPost={editPost}
     />
   ));
 };
